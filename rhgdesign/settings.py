711cccd46dcd465be4776346a959d9a08c010dcc
Ben Gosney <bengosney@googlemail.com>
2017-08-21 22:20:39 +0100

    Don't create auth urls as the bucket is public

--- rhgdesign/settings.py
+++ rhgdesign/settings.py
@@ -123,6 +123,7 @@ THUMBNAIL_DEFAULT_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'rhgd'
+AWS_QUERYSTRING_AUTH=False
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
