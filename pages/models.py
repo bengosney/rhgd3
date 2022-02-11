4a0889f94c230e1739ea094be64e0724986be99e
Ben Gosney <bengosney@googlemail.com>
2022-02-11 11:31:15 +0000

    ugettext is depricated

--- pages/models.py
+++ pages/models.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from django.db import models
 from django.urls import reverse
 from django.utils.functional import lazy
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 # Third Party
 from ckeditor_uploader.fields import RichTextUploadingField as RichTextField
